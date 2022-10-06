b2cf2c791658669f6d51d7e2b9766d420c32ec1b
ssvishal1990 <ssvishal1990@gmail.com>
2022-10-06 10:51:17 +0530

    Start of implementation of basic grid system

---
+++ Assets/Scripts/Testing.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    private GridSystem gridSystem;
+    void Start()
+    {
+        gridSystem = new GridSystem(10, 10, 2f);
+        Debug.Log(new GridPosition(5, 7));
+    }
+
+    private void Update()
+    {
+        Debug.Log(gridSystem.GetGridPosition(MouseWorld.GetPosition()));
+    }
+
+}
